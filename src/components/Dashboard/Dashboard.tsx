d88e7c798707f24301d3175dda0254de687fbaf7
Tetiana <bangchanka0310@gmail.com>
2026-07-24 21:50:26 +0300

    add corrections with dynamic instructions

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -5,19 +5,43 @@ import { FaArrowRight } from 'react-icons/fa';
 import css from './Dashboard.module.css';
 
 export default function Dashboard() {
+  const steps = [
+    {
+      id: 1,
+      title: 'Create a personal library: ',
+      text: 'add the books you intend to read to it.',
+    },
+    {
+      id: 2,
+      title: 'Create your first workout: ',
+      text: 'define a goal, choose a period, start training.',
+    },
+  ];
   return (
     <div className={css.wrapper}>
       <Filters />
       <div className={css.wrapperDescr}>
         <h3 className={css.title}>Start your workout</h3>
-        <p className={css.text}>
+        <div className={css.steps}>
+          {steps.map(({ id, title, text }) => (
+            <div key={id} className={css.step}>
+              <div className={css.number}>{id}</div>
+              <p className={css.text}>
+                <span className={css.marker}>{title}</span>
+                {text}
+              </p>
+            </div>
+          ))}
+        </div>
+
+        {/* <p className={css.text}>
           <span className={css.marker}>Create a personal library: </span>
           add the books you intend to read to it.
         </p>
         <p className={css.text}>
           <span className={css.marker}>Create your first workout: </span>
           define a goal, choose a period, start training.
-        </p>
+        </p> */}
         <div className={css.linkBlock}>
           <Link to="/library" className={css.link}>
             My library
--- src/components/RecommendedBooks/RecommendedBooks.tsx
+++ src/components/RecommendedBooks/RecommendedBooks.tsx
@@ -1,3 +1,4 @@
+import Dashboard from '../Dashboard/Dashboard.js';
 import css from './RecommendedBooks.module.css';
 
 export default function RecommendedBooks() {
