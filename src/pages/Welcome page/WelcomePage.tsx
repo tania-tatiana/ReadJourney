2cd996a76590ded4405f099f7c58cf0a86fada18
Tetiana <bangchanka0310@gmail.com>
2026-08-02 22:14:44 +0300

    add welcome page to mobile

--- src/pages/Welcome page/WelcomePage.tsx
+++ src/pages/Welcome page/WelcomePage.tsx
@@ -1,3 +1,22 @@
+import { Link } from 'react-router-dom';
+import css from './WelcomePage.module.css';
+
 export default function WelcomePage() {
-  return <></>;
+  return (
+    <section className={css.wrapper}>
+      <h1 className={css.title}>Welcome to Read Journey</h1>
+      <p className={css.text}>
+        Keep track of your reading, organize your library and achieve your
+        reading goals.
+      </p>
+      <div className={css.actions}>
+        <Link to="/login" className={css.loginButton}>
+          Log in
+        </Link>
+        <Link to="/register" className={css.registerButton}>
+          Create an account
+        </Link>
+      </div>
+    </section>
+  );
 }
