3e2e735667c1ba1d43ab778c0ce133d42cb2afaf
Tetiana <bangchanka0310@gmail.com>
2026-08-19 22:33:11 +0300

    start writing funtion with fetch

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-type SingUpData = {
+type SignUpData = {
   name: string;
   email: string;
   password: string;
@@ -10,3 +10,17 @@ type AuthResponse = {
   token: string;
   refreshToken: string;
 };
+
+const baseURL = 'https://readjourney.b.goit.study/api';
+
+export default async function signUp(data: SignUpData) {
+  let response = await fetch(`${baseURL}/users/signup`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  let result = await response.json();
+
+  return result;
+}
