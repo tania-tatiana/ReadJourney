c741910ca1c0c4172f8da0e9bfbfd25517cbf736
Tetiana <bangchanka0310@gmail.com>
2026-09-01 20:57:37 +0300

    use function getCurrentUser in the UserBar

--- src/components/UserBar/UserBar.tsx
+++ src/components/UserBar/UserBar.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import css from './UserBar.module.css';
+import { getCurrentUser, type GetCurrentUser } from '../../services/api.js';
 
 export default function UserBar() {
-  const username = 'Ilona Ratushniak';
+  const [user, setUser] = useState<GetCurrentUser | null>(null);
+  useEffect(() => {
+    async function getUser() {
+      const result = await getCurrentUser();
+      setUser(result);
+    }
+    getUser();
+  }, []);
+  if (!user) {
+    return null;
+  }
   return (
     <div className={css.userBar}>
-      <div className={css.avatar}>{username.charAt(0).toUpperCase()}</div>
-      <p className={css.user}>{username}</p>
+      <div className={css.avatar}>{user.name.charAt(0).toUpperCase()}</div>
+      <p className={css.user}>{user.name}</p>
     </div>
   );
 }
