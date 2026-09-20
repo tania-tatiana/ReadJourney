0c713f56d54a0abdc84652a9c322d44fe416510a
Tetiana <bangchanka0310@gmail.com>
2026-09-20 17:51:11 +0300

    start implementing backend component

--- src/components/AddBook/AddBook.tsx
+++ src/components/AddBook/AddBook.tsx
@@ -4,30 +4,31 @@ import css from './AddBook.module.css';
 
 export default function AddBook() {
   const [isAddBookModalOpen, setIsAddBookModalOpen] = useState(false);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+  }
   return (
     <div className={css.wrapper}>
       <p className={css.title}>Create your library:</p>
-      <form className={css.form}>
+      <form className={css.form} onSubmit={handleSubmit}>
         <div className={css.inputs}>
           <label className={css.field}>
             <span className={css.label}>Book title:</span>
-            <input type="text" className={css.input} />
+            <input type="text" className={css.input} name="title" />
           </label>
           <label className={css.field}>
             <span className={css.label}>The author:</span>
-            <input type="text" className={css.input} />
+            <input type="text" className={css.input} name="author" />
           </label>
           <label className={css.field}>
             <span className={css.label}>Number of pages:</span>
-            <input type="text" className={css.input} />
+            <input type="text" className={css.input} name="totalPages" />
           </label>
         </div>
 
-        <button
-          type="submit"
-          className={css.button}
-          onClick={() => setIsAddBookModalOpen(true)}
-        >
+        <button type="submit" className={css.button}>
           Add book
         </button>
       </form>
