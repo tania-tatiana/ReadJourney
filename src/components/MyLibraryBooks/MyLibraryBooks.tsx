a557cba3a6da6fbbc2d87d3619f5db6d91cff6d6
Tetiana <bangchanka0310@gmail.com>
2026-09-23 19:11:06 +0300

    continue implement backend to MyLibraryBooks

--- src/components/MyLibraryBooks/MyLibraryBooks.tsx
+++ src/components/MyLibraryBooks/MyLibraryBooks.tsx
@@ -1,22 +1,42 @@
-import LibrarySelect from '../LibrarySelect/LibrarySelect.js';
+import { useEffect, useState } from 'react';
 import css from './MyLibraryBooks.module.css';
+import { getLibraryBooks, type LibraryBooks } from '../../services/api.js';
+import LibrarySelect from '../LibrarySelect/LibrarySelect.js';
 
 export default function MyLibraryBooks() {
+  const [status, setStatus] = useState('All books');
+  const [books, setBooks] = useState<LibraryBooks>([]);
+
+  useEffect(() => {
+    async function fetchBooks() {
+      const result = await getLibraryBooks(status);
+      setBooks(result);
+    }
+    fetchBooks();
+  }, [status]);
+
   return (
-    <div className={css.wrapper}>
-      <div className={css.titleAndFilters}>
-        <h2 className={css.title}>My library</h2>
-        <LibrarySelect />
-      </div>
-      <div className={css.pictureAndText}>
-        <div className={css.circle}>
-          <img src="./bigBooks.png" alt="Books" className={css.image} />
+    <>
+      {books.length > 0 ? (
+        'books'
+      ) : (
+        <div className={css.wrapper}>
+          <div className={css.titleAndFilters}>
+            <h2 className={css.title}>My library</h2>
+            <LibrarySelect />
+          </div>
+          <div className={css.pictureAndText}>
+            <div className={css.circle}>
+              <img src="./bigBooks.png" alt="Books" className={css.image} />
+            </div>
+            <p className={css.marker}>
+              <span className={css.text}>To start training, add</span> some of
+              your books{' '}
+              <span className={css.text}>or from the recommended ones</span>
+            </p>
+          </div>
         </div>
-        <p className={css.marker}>
-          <span className={css.text}>To start training, add</span> some of your
-          books <span className={css.text}>or from the recommended ones</span>
-        </p>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
