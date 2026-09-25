76198e188c4d535c1eb161ea39b4116fae773940
Tetiana <bangchanka0310@gmail.com>
2026-09-25 09:15:40 +0300

    add styles for LibraryBookCard

--- src/components/LibraryBookCard/LibraryBookCard.tsx
+++ src/components/LibraryBookCard/LibraryBookCard.tsx
@@ -21,7 +21,9 @@ export default function LibraryBookCard({
           <p className={css.title}>{title}</p>
           <p className={css.author}>{author}</p>
         </div>
-        <button type="button" className={css.deleteBuuton}></button>
+        <button type="button" className={css.deleteButton}>
+          <img src="../../../public/deleteButton.svg" alt="Delete button" />
+        </button>
       </div>
     </div>
   );
